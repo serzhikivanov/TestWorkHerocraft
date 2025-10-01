1caac184edc7d1c9ce8c2dece161e4c0ffcae5a9
Sergey Ivanov <serzhik.ivanov@gmail.com>
2025-10-01 14:31:27 +0300

    missing changes

--- ChessSrvClient/Configuration/ChessClientSettings.cs
+++ ChessSrvClient/Configuration/ChessClientSettings.cs
@@ -2,5 +2,9 @@
 
 public class ChessClientSettings
 {
-    public string Url { get; set; }
+    public string Url { get; set; } = string.Empty;
+    
+    public string UrlRaw { get; set; } = string.Empty;
+
+    public string PortRaw { get; set; } = string.Empty;
 }
--- ChessSrvClient/Controllers/ChessMoveController.cs
+++ ChessSrvClient/Controllers/ChessMoveController.cs
@@ -1,6 +1,7 @@
 using ChessSrvClient.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 
@@ -21,8 +22,8 @@ public class ChessMoveController : ControllerBase
         _options = options;
     }
 
-    [HttpGet(Name = "GetKnightMoves")]
-    public async Task<string> Get([FromQuery] string startCell, [FromQuery] string endCell)
+    [HttpPost(Name = "GetKnightMoves")]
+    public async Task<string> GetKnightMoves([FromQuery] string startCell, [FromQuery] string endCell)
     {
         var requestObj = new
         {
@@ -41,5 +42,26 @@ public class ChessMoveController : ControllerBase
         _logger.LogInformation("Received response {0}", response);
 
         return responseBody;
+    }
+
+    [HttpPost("GetKnightMovesRaw")]
+    public async Task<string> GetKnightMovesRaw([FromQuery] string startCell, [FromQuery] string endCell)
+    {
+        var requestObj = new { from = startCell, to = endCell };
+        var json = JsonSerializer.Serialize(requestObj);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        _logger.LogInformation("Sending RAW request {0} to {1}",
+            json, _options.Value.UrlRaw);
+
+        using var client = new TcpClient();
+        await client.ConnectAsync(_options.Value.UrlRaw, int.Parse(_options.Value.PortRaw));
+        var stream = client.GetStream();
+        var data = Encoding.UTF8.GetBytes(json);
+        await stream.WriteAsync(data);
+        await stream.FlushAsync();
+
+        var buffer = new byte[4096];
+        int read = await stream.ReadAsync(buffer);
+        return Encoding.UTF8.GetString(buffer, 0, read);
     }
 }
